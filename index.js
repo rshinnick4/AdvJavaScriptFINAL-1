2ef101f8e2542882d57607a4efec7b5cc158f35b
KorayTA <koraya527@gmail.com>
2021-04-25 16:51:44 -0400

    mongoFiles

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var signIn = require('./HTML/Sign-in.html')
+//var signIn = require('./HTML/Sign-in.html')
 
-var signIn = function(req, res) {
+//var signIn = function(req, res) {
 
-}
+//}
+
+var MongoClient = require('users').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/main', function(err, db) {
+    if(err) throw(err);
+    var users = db.getCollection('users')
+
+
+  });
